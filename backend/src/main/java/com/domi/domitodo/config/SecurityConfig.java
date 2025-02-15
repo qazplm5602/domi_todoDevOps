6d08cdc02fd96d20c893b7b5756da1c863afb2a4
domi <domi@domi.kr>
2025-02-16 02:24:52 +0900

    백엔드 시큐리티 설정

---
+++ backend/src/main/java/com/domi/domitodo/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.domi.domitodo.config;
+
+import com.domi.domitodo.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable); // csrf 비활
+
+        // 세션 id 사용 안함
+        http.sessionManagement(v ->
+                v.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        
+        http.logout(AbstractHttpConfigurer::disable);
+        
+        http.authorizeHttpRequests(
+                request ->
+                request.anyRequest().permitAll() // 일단 모든 주소 허용
+        );
+
+        http.addFilter(new JwtAuthenticationFilter());
+
+        return http.build();
+    }
+}
