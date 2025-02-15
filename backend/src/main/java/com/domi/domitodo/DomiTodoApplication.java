7299e48d9d5aed99acf4f1e28f8fd23938bd79b4
domi <domi@domi.kr>
2025-02-16 01:41:47 +0900

    백엔드 init

---
+++ backend/src/main/java/com/domi/domitodo/DomiTodoApplication.java
@@ -0,0 +1,13 @@
+package com.domi.domitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomiTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomiTodoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/domi/domitodo/DomiTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domi.domitodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomiTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
