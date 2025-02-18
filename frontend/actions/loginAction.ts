3e2dcb486bb0f094a801b90e837d20daab03d9d8
domi <domi@domi.kr>
2025-02-18 22:44:31 +0900

    프론트 로그인 토큰 받아오기

--- frontend/actions/loginAction.ts
+++ frontend/actions/loginAction.ts
@@ -1,7 +1,13 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { request } from "../hooks/request";
 
+class LoginError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
 
 export async function loginAction(form: FormData) {
     const email = form.get('email');
@@ -10,5 +16,24 @@ export async function loginAction(form: FormData) {
     if (email === '' || password === '')
         return "이메일 및 비밀번호를 입력하세요."
 
+    try {
+        await apiTokenHandler(email.toString(), password.toString());
+    } catch (e) {
+        if (e instanceof LoginError) {
+            return e.message;
+        } else {
+            throw e;
+        }
+    }
+
     redirect("/");
+}
+
+export async function apiTokenHandler(email: string, password: string) {
+    const { code, data } = await request("/login", { method: "POST", tokenDisable: true, headers: { 'Content-Type': "application/json" }, body: JSON.stringify({email, password}) });
+    if (code !== 200) { // 로그인 실패 (서버가 터졋거나 잘못 입력햇거나 ㅁㄴㅇㄹ)
+        throw new LoginError("로그인 오류");
+    }
+    
+    console.log(code, data);
 }
