e9a803f285627e907b134c28df7c87edd0d76c6d
domi <domi@domi.kr>
2025-02-19 01:15:27 +0900

    프론트 로그인 액션

--- frontend/actions/loginAction.ts
+++ frontend/actions/loginAction.ts
@@ -1,12 +1,18 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { request } from "../hooks/request";
+import { ApiError, request } from "../hooks/request";
+import { cookies } from "next/headers";
 
 class LoginError extends Error {
     constructor(message) {
         super(message);
     }
+}
+
+interface AuthTokenVO {
+    access: string,
+    refresh: string
 }
 
 export async function loginAction(form: FormData) {
@@ -25,15 +31,23 @@ export async function loginAction(form: FormData) {
             throw e;
         }
     }
-
-    redirect("/");
 }
 
 export async function apiTokenHandler(email: string, password: string) {
-    const { code, data } = await request("/login", { method: "POST", tokenDisable: true, headers: { 'Content-Type': "application/json" }, body: JSON.stringify({email, password}) });
-    console.log(code, data);
+    const { code, data } = await request<any>("/login", { method: "POST", tokenDisable: true, headers: { 'Content-Type': "application/json" }, body: JSON.stringify({email, password}) });
 
     if (code !== 200) { // 로그인 실패 (서버가 터졋거나 잘못 입력햇거나 ㅁㄴㅇㄹ)
-        throw new LoginError("로그인 오류");
+        if (data.code !== undefined)
+            throw new LoginError((data as ApiError).message);
+
+        throw new Error("API 서버 오류");
     }
+
+    const token = data as AuthTokenVO;
+    const cookie = await cookies();
+    cookie.set("accessToken", token.access, { httpOnly: true });
+    cookie.set("refreshToken", token.refresh, { httpOnly: true });
+    
+    // 끗
+    redirect("/");
 }
