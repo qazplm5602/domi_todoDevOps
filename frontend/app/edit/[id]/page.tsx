174e6bf5ada3bd4bb4143e98cc38edc98f323d0a
domi <domi@domi.kr>
2025-02-20 23:21:12 +0900

    Update page.tsx

--- frontend/app/edit/[id]/page.tsx
+++ frontend/app/edit/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { Params } from "next/dist/server/request/params";
 import TodoForm from "../../../components/TodoForm/TodoForm";
 
-export default function Page() {
+export default async function Page({ params }: { params: Params }) {
+    const { id } = await params;
+
     return <TodoForm />
 }
